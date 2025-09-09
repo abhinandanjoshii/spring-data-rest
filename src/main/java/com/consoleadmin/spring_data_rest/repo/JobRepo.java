ee585226c97edfed3208a0304478543fbdebf56b
abhinandanjoshii <113920019+abhinandanjoshii@users.noreply.github.com>
2025-09-09 12:00:12 +0530

    normie proj

---
+++ src/main/java/com/consoleadmin/spring_data_rest/repo/JobRepo.java
@@ -0,0 +1,14 @@
+package com.consoleadmin.spring_data_rest.repo;
+import com.consoleadmin.spring_data_rest.model.JobPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface JobRepo extends JpaRepository<JobPost, Integer> {
+
+    //List<JobPost> findByPostProfileContainingOrPostDescContaining(String postProfile, String postDesc);
+
+
+}
