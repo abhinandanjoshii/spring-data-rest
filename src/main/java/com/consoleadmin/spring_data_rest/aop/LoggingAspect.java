7a859dc3acc1cbdff73ba0c014ed6f2183e224e7
abhinandanjoshii <113920019+abhinandanjoshii@users.noreply.github.com>
2025-09-09 16:00:20 +0530

    after advice

--- src/main/java/com/consoleadmin/spring_data_rest/aop/LoggingAspect.java
+++ src/main/java/com/consoleadmin/spring_data_rest/aop/LoggingAspect.java
@@ -1,7 +1,7 @@
 package com.consoleadmin.spring_data_rest.aop;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -15,7 +15,23 @@ public class LoggingAspect {
     // return type, class-name.method-name(arguments)
     @Before("execution(* com.consoleadmin.spring_data_rest.service.JobService.*(..))")
 //    @Before("execution(* com.consoleadmin.spring_data_rest.service.JobService(..))")
-    private void logMethodCall(){
-        LOGGER.info("Method Called");
+    private void logMethodCall(JoinPoint jp){
+        LOGGER.info("Method Called " + jp.getSignature().getName());
+    }
+
+    // irresespective of exception, finally kinda
+    @After("execution(* com.consoleadmin.spring_data_rest.service.JobService.*(..))")
+    private void logMethodExecuted(JoinPoint jp){
+        LOGGER.info("Method Leaved Thread " + jp.getSignature().getName());
+    }
+
+    @AfterReturning("execution(* com.consoleadmin.spring_data_rest.service.JobService.*(..))")
+    private void logMethodExecutedSuccess(JoinPoint jp){
+        LOGGER.info("Method Executed Successfully " + jp.getSignature().getName());
+    }
+
+    @AfterThrowing("execution(* com.consoleadmin.spring_data_rest.service.JobService.*(..))")
+    private void logMethodCrash(JoinPoint jp){
+        LOGGER.info("Method Crashed " + jp.getSignature().getName());
     }
 }
